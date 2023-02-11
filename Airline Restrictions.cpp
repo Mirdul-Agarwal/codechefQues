f4857b89e442523f4d6abcf8f5d4445414af8403
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-02-11 22:45:21 +0530

    Create Airline Restrictions.cpp

---
+++ Airline Restrictions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d,e;
+	    cin>>a>>b>>c>>d>>e;
+	    if((a+b<=d && c<=e) || (a+c<=d && b<=e) || (b+c<=d && a<=e)){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;	    }
+	    
+	}
+	return 0;
+}
