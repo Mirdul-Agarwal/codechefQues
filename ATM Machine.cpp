61084827255ed49702c37dfbeab6ebd537e0fcf2
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-02-16 23:15:23 +0530

    Create ATM Machine.cpp

---
+++ ATM Machine.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while(t--){
+	    int n,k,total=0;
+	    cin >>n>>k;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin >>arr[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        if(arr[i]<=k){
+	            cout <<"1";
+	            k-=arr[i];
+	        }else{
+	            cout <<"0";
+	        }
+	    }
+	    cout <<endl;
+	    
+	}
+	return 0;
+}
