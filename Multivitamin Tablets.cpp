f07ccdaa794951bc615637b1956c85075361647a
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-03-14 16:45:47 +0530

    Create Multivitamin Tablets.cpp

---
+++ Multivitamin Tablets.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(y>=x*3){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
