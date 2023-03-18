3d1c5075ee699bb88e0e3b9fc722b6338069e7ea
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-03-18 23:47:39 +0530

    Create Chef Drinks Tea.cpp

---
+++ Chef Drinks Tea.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if(x%y==0)
+	    cout<<(x/y)*z<<endl;
+	    else
+	    cout<<((x/y)+1)*z<<endl;
+	    
+	}
+	return 0;
+}
