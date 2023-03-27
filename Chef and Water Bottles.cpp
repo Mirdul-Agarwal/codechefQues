974b01f5307d1a75e37aad71d37d24dde130ee9a
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-03-27 14:04:09 +0530

    Create Chef and Water Bottles.cpp

---
+++ Chef and Water Bottles.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,k;
+	    cin>>n>>x>>k;
+	    if((n*x)>=k){
+	        cout<<k/x<<endl;
+	    }
+	    else{
+	        cout<<n<<endl;
+	    }
+	}
+	return 0;
+}
