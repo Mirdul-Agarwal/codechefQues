39b252c5cad6d46909f57758381d4fcbb7235f1f
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-03-29 14:19:23 +0530

    Create Candy Distribution.cpp

---
+++ Candy Distribution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m,temp;
+	    cin>>n>>m;
+	    temp=m*2;
+	    if(n%temp==0){
+	      
+	            cout<<"YES"<<endl;
+	            
+	        
+	   
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
