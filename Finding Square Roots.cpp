fb247a548e868df1554b22c1dd52d3a370a1f309
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-03-30 23:23:37 +0530

    Create Finding Square Roots.cpp

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+         cin >> n;
+         cout << int(sqrt(n)) << endl;
+    }
+    return 0;
+}
