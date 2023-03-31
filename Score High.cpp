943c670f9e7407e64d9659b743a8af6f1646d7fe
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-03-31 23:09:41 +0530

    Create Score High.cpp

---
+++ Score High.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int test;
+	cin>>test;
+	while(test--){
+	    int n;
+	    cin>>n;
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    cout<<max({a,b,c,d})<<endl;
+	    
+	}
+	return 0;
+}
