c58d90f427e78584f51387be3be36424868163b3
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-04-09 22:56:53 +0530

    Create Easy Permutation.cpp

---
+++ Easy Permutation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    for(int i=n;i>0;i--)
+	        cout<<i<<" ";
+	    cout<<endl;
+	}
+	return 0;
+}
