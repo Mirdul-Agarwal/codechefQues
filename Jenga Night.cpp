c3f157bef99cc69ca92ef890259e1fb4bc8e9702
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-04-10 23:39:01 +0530

    Create Jenga Night.cpp

---
+++ Jenga Night.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        if(x%n==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+	return 0;
+}
