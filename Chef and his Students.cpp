561b2b6d115e1615e649c2b3fb97c0e6af6bf56e
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-04-22 23:55:16 +0530

    Create Chef and his Students.cpp

---
+++ Chef and his Students.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin>>s;
+	    int count=0;
+	    for(int i=0;i<s.size();i++)
+	    {
+	        if(s[i]=='<' && s[i+1]=='>')
+	        count++;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
