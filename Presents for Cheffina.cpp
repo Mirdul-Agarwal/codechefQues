68923b522b60d4a1f7d68bbf3aa5ca82541c85c3
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-04-23 23:36:18 +0530

    Create Presents for Cheffina.cpp

---
+++ Presents for Cheffina.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	std::cin >> t;
+	while(t--)
+	{
+	     int n;
+	    std::cin >> n;
+	    int count=0;
+	 
+	    std::cout << n-n/5 << std::endl;
+	}
+	return 0;
+}
