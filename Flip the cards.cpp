43045a50291ad947cf651eb4748528d365315c5d
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-05-05 23:48:02 +0530

    Create Flip the cards.cpp

---
+++ Flip the cards.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    if(x==0 || n==x){
+	    cout<<0<<endl;
+	    }
+	    else if(n-x>x){
+	    cout<<x<<endl;
+	    }
+	    else{
+	    cout<<n-x<<endl;
+	    }
+	    
+	    
+	}
+	return 0;
+}
