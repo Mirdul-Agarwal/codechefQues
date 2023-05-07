f16175e01e5891ebea7e29d9615c430af1002dc1
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-05-07 22:05:15 +0530

    Create Masterchef finals.cpp

---
+++ Masterchef finals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >>t;
+	while(t--)
+	{
+	    int x;
+	    cin >>x ;
+	    cout << (x<=10?"Yes":"No") <<endl;
+	}
+	return 0;
+}
