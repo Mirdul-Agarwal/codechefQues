088f16eb841b1a30e3f49262d2a56b56ed5a973e
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-05-12 23:53:09 +0530

    Create Summer Heat.cpp

---
+++ Summer Heat.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--){
+      int xa,xb,Xa,Xb;
+      cin>>xa>>xb>>Xa>>Xb;
+      cout<<(Xa/xa)+(Xb/xb)<<endl;
+  }
+  return 0;
+}
