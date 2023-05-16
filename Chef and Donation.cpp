e2714fcf885ef3636e97fd1903f8fb1e4eef2745
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-05-16 23:56:26 +0530

    Create Chef and Donation.cpp

---
+++ Chef and Donation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    ll x,y;
+	    cin>>x>>y;
+	    cout<<(y-x)<<endl;
+	}
+	return 0;
+}
