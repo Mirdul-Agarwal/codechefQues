d51dc278b2f50bccd27776645e7f318a40f4c7ef
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-05-19 23:51:56 +0530

    Create Parliament.cpp

---
+++ Parliament.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    int c=b*2;
+	    if(c>=a)
+	    cout<<"YES\n";
+        else 
+        cout<<"NO\n";
+	    
+	    
+	}
+	return 0;
+}
