2ad1708a66e28134af1a672d7afffc3453e61334
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-05-26 21:52:23 +0530

    Create Bus Seat Numbering.cpp

---
+++ Bus Seat Numbering.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n>=1 && n<=10)           cout<<"Lower Double"<<endl;
+        else if(n>=11 && n<=15)     cout<<"Lower Single"<<endl;
+        else if(n>=16 && n<=25)     cout<<"Upper Double"<<endl;
+        else                        cout<<"Upper Single"<<endl;
+    }
+	return 0;
+}
