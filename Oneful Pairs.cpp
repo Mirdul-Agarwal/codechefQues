a2f759386a8c29b0a57fd2802bdc847904ff11dd
Mirdul_Agarwal_23 <107631413+Mirdul-Agarwal@users.noreply.github.com>
2023-06-01 21:20:42 +0530

    Create Oneful Pairs.cpp

---
+++ Oneful Pairs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a, b;
+	cin>>a>>b;
+	int c=a+b+(a*b);
+	if(c==111) cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+	return 0;
+}
